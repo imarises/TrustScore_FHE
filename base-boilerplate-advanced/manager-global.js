308516b01d6d2dbaa2f54ae194f9b0d433836343
imarises <imarises@users.noreply.github.com>
2025-11-04 15:01:02 +0000

    Add build-frontend-mock.java, manager-global.js, validator-factory-middleware.jsx

---
+++ base-boilerplate-advanced/manager-global.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+interface layoutsProps { data: string }
+
---
+++ base-boilerplate-advanced/validator-factory-middleware.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+export const environment_common_global = () => { return null; };
+
